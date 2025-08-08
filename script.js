19ae4e68fab89e21635082d325d830323e403005
ProxymityWorks <164102954+Hopendo@users.noreply.github.com>
2025-08-08 15:23:24 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+const track = document.querySelector('.carousel-track');
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  // Mouse and Touch Drag
+  track.addEventListener('mousedown', (e) => {
+    isDown = true;
+    track.classList.add('active');
+    startX = e.pageX - track.offsetLeft;
+    scrollLeft = track.scrollLeft;
+  });
+
+  track.addEventListener('mouseleave', () => {
+    isDown = false;
+    track.classList.remove('active');
+  });
+
+  track.addEventListener('mouseup', () => {
+    isDown = false;
+    track.classList.remove('active');
+  });
+
+  track.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - track.offsetLeft;
+    const walk = (x - startX) * 1.5; // speed multiplier
+    track.scrollLeft = scrollLeft - walk;
+  });
